9e9564f195e6e3da4ccf902e3d61b655eee44bdd
vishwanuwan2002 <vishwanuwan200205@gmail.com>
2026-05-02 23:04:45 +0530

    Update website title

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Portfolio-Website/",
+  base: mode === 'production' ? "/Portfolio-Website/" : "/",
   server: {
     host: "::",
     port: 8080,
